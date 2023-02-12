3211ceb01f7c55d8ef89b976dce1e1f6b4bf0676
Victoria Chen <victoriachen4520@gmail.com>
2023-02-11 23:38:25 -0500

    skeleton for manager

--- src/roster/RosterManager.java
+++ src/roster/RosterManager.java
@@ -1,7 +1,50 @@
 package roster;
+import java.util.Scanner;
 
 public class RosterManager {
     public void run() {
-        // run the roster manager!
+        Scanner intake = new Scanner(System.in);
+        // Roster studentRoster = new Roster();
+        String opCode = "init";
+        System.out.println("Roster manager running...");
+
+        while (!opCode.equals("Q")) {
+            String command = intake.nextLine();
+            System.out.println(command);
+            String[] parameters = command.split(" ");
+            opCode = parameters[0];
+            System.out.println("Opcode is " + opCode);
+
+            // Course of action based on op code
+            switch (opCode) {
+                case "A":
+                    System.out.println("Add student");
+                    break;
+                case "R":
+                    System.out.println("Remove student");
+                    break;
+                case "P":
+                    System.out.println("Display roster by last name");
+                    break;
+                case "PS":
+                    System.out.println("Display roster by standing");
+                    break;
+                case "PC":
+                    System.out.println("Display roster by school/major");
+                    break;
+                case "L":
+                    System.out.println("List students in a school");
+                    break;
+                case "C":
+                    System.out.println("Change a students major");
+                    break;
+                case "Q":
+                    System.out.println("Roster Manager terminated.");
+                    break;
+                default:
+                    System.out.println(opCode + " is an invalid command!");
+            }
+        }
+
     }
 }
