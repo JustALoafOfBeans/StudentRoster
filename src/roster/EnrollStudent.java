ad3c6dac3e863088944b52c7025733b19e5676c9
brizbird14 <brizbird14@gmail.com>
2023-02-26 14:59:01 -0500

    EnrollStudent and Enrollment things

--- src/roster/EnrollStudent.java
+++ src/roster/EnrollStudent.java
@@ -3,4 +3,49 @@ package roster;
 public class EnrollStudent {
     private Profile profile;
     private int creditsEnrolled;
+
+    public EnrollStudent(String enrollInput) { // Constructor
+        // todo check input, takes in String eg. "Emma Miller 2/28/2003 8"
+        // Expect profile to conclude just before 3rd space
+        String profileStr = "", creditStr = ""; // todo is this syntax legal for Chang
+        int spaceCount = 0;
+        for (int ind = 0; ind < enrollInput.length(); ind++) {
+            if (enrollInput.charAt(ind) == ' ') { // Look for " " delimiter
+                spaceCount += 1;
+            }
+            if (spaceCount == 3) { // Encountered 3rd space
+                profileStr = enrollInput.substring(0, ind);
+                // Expect credits after space following profile, goes to end
+                creditStr = enrollInput.substring(ind+1);
+            }
+        }
+        profile = new Profile(profileStr);
+        creditsEnrolled = Integer.parseInt(creditStr);
+    }
+
+    @Override
+    public String toString() {
+        // Format: [profile as FIRST LAST DOB]: credits enrolled: [credits]
+        String strEnroll = profile.toString();
+        strEnroll += ": credits enrolled: ";
+        strEnroll += creditsEnrolled;
+        return strEnroll;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public int getCredits() {
+        return creditsEnrolled;
+    }
+
+    public void setCredits(int newCredits) {
+        creditsEnrolled = newCredits;
+    }
+
+    public static void main (String[] args) {
+        EnrollStudent testEnroll = new EnrollStudent("Emma Miller 2/28/2003 8");
+        System.out.println(testEnroll.toString());
+    }
 }
--- src/roster/Enrollment.java
+++ src/roster/Enrollment.java
@@ -3,19 +3,77 @@ package roster;
 public class Enrollment {
     private EnrollStudent[] enrollStudents;
     private int size;
+
+    private static final int STARTSIZE = 0;
+
+    public Enrollment() {
+        size = STARTSIZE;
+        enrollStudents = new EnrollStudent[size];
+    }
+
     public void add(EnrollStudent enrollStudent) {
-        // todo
+        // Check if student in roster (if not, print error) // todo allowed to print here??
+        // Check to see if student already enrolled
+        int foundIndex = find(enrollStudent);
+        if (foundIndex >= 0) {
+            // Student found in enrollment, update credits but do not add
+            enrollStudents[foundIndex].setCredits(enrollStudent.getCredits());
+        } else {
+            if (enrollStudents.length == size) { // Enrollment full, resize
+                grow();
+            }
+            enrollStudents[size] = enrollStudent; // Add to end
+            size += 1; // Increase count
+        }
+    }
+
+    private void grow () {
+        // Increase array capacity by 4
+        EnrollStudent[] enrollStudentsNew = new EnrollStudent[enrollStudents.length + 4];
+        for (int ind = 0; ind < size; ind++) {
+            enrollStudentsNew[ind] = enrollStudents[ind];
+        }
+        enrollStudents = enrollStudentsNew;
     }
 
     public void remove(EnrollStudent enrollStudent) {
-        // todo
+        int remIndex = find(enrollStudent); // Index to remove at
+        if (remIndex < 0) { // Not in enrollment, nothing to remove
+            return; // todo error?
+        }
+        size -= 1; // Decrease enrollment count
+        // For every index at and beyond remIndex, move up
+        for (int ind = remIndex; ind < enrollStudents.length; ind++) {
+            if (ind == enrollStudents.length-1 || enrollStudents[ind + 1] == null) {
+                // At end (of array or of non-null indices in array)
+                enrollStudents[ind] = null;
+                break;
+            }
+            enrollStudents[ind] = enrollStudents[ind+1];
+        }
+    }
+
+    // Helper method for finding student index, used by add(), remove(), and find()
+    private int find(EnrollStudent enrollStudent) {
+        Profile seekProf = enrollStudent.getProfile(); // Profile of enrollStudent sought
+        for (int ind = 0; ind < enrollStudents.length; ind++) {
+            if (seekProf.compareTo(enrollStudents[ind].getProfile()) == 0) {
+                // Found, return index
+                return ind;
+            }
+        }
+        return -1; // Not found
     }
 
     public boolean contains(EnrollStudent enrollStudent) {
-        return false; // temp return to avoid error mark todo
+        int containsIndex = find(enrollStudent);
+        // Returns "true" if in enrollment at some index >=0 containsIndex
+        return (containsIndex >= 0);
     }
 
     public void print() {
-        // todo
+        for (int ind = 0; ind < enrollStudents.length; ind++) {
+            System.out.println(enrollStudents[ind].toString());
+        }
     }
 }
