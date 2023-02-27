e23a1f3f32c6da93aa28cbb2803997e3729aa657
brizbird14 <brizbird14@gmail.com>
2023-02-27 18:59:33 -0500

    Setup international and roster tests

---
+++ src/roster/InternationalTest.java
@@ -0,0 +1,18 @@
+package roster;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InternationalTest {
+
+    @Test
+    public void test_tuitionDue_notAbroad() {
+
+    }
+
+    @Test
+    public void test_tuitionDue_notAbroad() {
+
+    }
+}
